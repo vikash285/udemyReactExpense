24338935a1afbc482a98b3aac00fc662deeec943
vikash285 <vikashdhaka285@gmail.com>
2023-10-10 12:53:27 +0530

    ExpenseDate js and css, ExpenseDetails component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
+import ExpenseDetails from "./ExpenseDetails";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>
-          {props.title} at {props.location}
-        </h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        title={props.title}
+        location={props.location}
+        amount={props.amount}
+      />
     </div>
   );
 }
