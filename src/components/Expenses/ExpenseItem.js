13d0638749b9cb5dcf22d74163bfd0dd67c3adc1
vikash285 <vikashdhaka285@gmail.com>
2023-11-16 15:59:05 +0530

    State introduced, title and amount changed

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,27 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDetails from "./ExpenseDetails";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
+
   const clickHandler = () => {
-    console.log("clicked!");
+    setTitle("Updated!");
+    console.log(title);
   };
-  const deleteHandler = () => {
-    console.log("Delete!");
+  const clickHandler1 = () => {
+    setAmount("100");
+    console.log(amount);
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <ExpenseDetails
-        title={props.title}
-        location={props.location}
-        amount={props.amount}
-      />
+      <ExpenseDetails title={title} location={props.location} amount={amount} />
       <button onClick={clickHandler}>Change Title</button>
-      <button onClick={deleteHandler}>Delete</button>
+      <button onClick={clickHandler1}>Up Amount</button>
     </Card>
   );
 };
