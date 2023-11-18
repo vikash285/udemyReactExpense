e9f08b5f11ec9725f7ca334e2dd26a8a55218c13
vikash285 <vikashdhaka285@gmail.com>
2023-11-18 19:10:20 +0530

    bud fix in form, new amount as number not string

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -32,7 +32,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
     props.onSaveExpenseData(expenseData);
