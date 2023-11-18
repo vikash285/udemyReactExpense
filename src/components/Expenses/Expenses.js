123b498c25c2af9a57a984c31743432b551189a1
vikash285 <vikashdhaka285@gmail.com>
2023-11-18 13:33:43 +0530

    conditional rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,21 +13,37 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  let expensesContent = <p>No Expense Found!</p>;
+  if (filteredExpenses.length === 1) {
+    expensesContent = [
+      <ExpenseItem
+        key={filteredExpenses[0].id}
+        title={filteredExpenses[0].title}
+        amount={filteredExpenses[0].amount}
+        date={filteredExpenses[0].date}
+        location={filteredExpenses[0].location}
+      />,
+      <p>Only single expense, add more...</p>,
+    ];
+  } else if (filteredExpenses.length > 1) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        location={expense.location}
+      />
+    ));
+  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          location={expense.location}
-        />
-      ))}
+      {expensesContent}
     </Card>
   );
 };
